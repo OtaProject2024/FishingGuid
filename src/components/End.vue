1ed71d63f5b9ab861f30f55a9aba4cf3fca4f2c7
kuma <kuyukishi2002@gmail.com>
2024-10-23 10:52:26 +0900

    v-0.8

--- src/components/End.vue
+++ src/components/End.vue
@@ -1,10 +1,6 @@
 <!--Chapter.vue-->
 <template>
   <div>
-      <div class="flex justify-start">
-          <div class="bg-white w-8 h-16 border border-gray-300 shadow-md relative ml-44"></div>
-          <h1 class="text-3xl font-bold ml-4 my-4 whitespace-pre-line">{{ title }}</h1>
-      </div>
       <div class="flex justify-center">
           <p class="text-left text-1xl font-semibold leading-loose w-2/3 my-6 mb-16 whitespace-pre-line">
               <span v-for="(part, index) in formattedText" :key="index">
