e45c619fb3ad25daa6105ab37654e9ce4c0b787e
ryokpen87044 <106538588+ryokpen87044@users.noreply.github.com>
2024-10-25 03:20:06 +0900

    Fix display

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,7 +16,7 @@
              font-bold">
       <router-link to="/" class="text-black">ホーム</router-link>
       <router-link to="/#Fish" class="text-black">釣りについて</router-link>
-      <router-link to="/#How-To" class="text-black">釣りについて</router-link>
+      <router-link to="/#How-To" class="text-black">釣り方</router-link>
       <img src="@/assets/img/logo.png" alt="Fish" class="px-8">
     </nav>
   </header>
